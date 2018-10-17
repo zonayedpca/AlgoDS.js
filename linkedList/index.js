16afc1c7c297e93406350f62c0b8bb1f1efa9ef4
Zonayed Ahmed <zonayedpca@yahoo.com>
2018-10-17 07:56:39 +0530

    linked list updated

--- linkedList/index.js
+++ linkedList/index.js
@@ -21,3 +21,33 @@ console.log(l1.getSize());
 
 // Get the First Node
 console.log(l1.getFirst());
+
+// Get the Last Node
+console.log(l1.getLast());
+
+// Remove the First node
+l1.removeFirst();
+console.log('Link List', l1);
+
+// Remove Last Node
+l1.removeLast();
+console.log('After Removing the Last Node', l1);
+
+// Insert Last
+l1.insertLast(15);
+console.log('After inserting a new Node at Last', l1);
+
+// Get Node from a given index
+console.log('getAt', l1.getAt(1));
+
+// Remove a Node from by given index
+l1.removeAt(1);
+console.log('After Removing the Last Node', l1);
+
+// Insert a Node at a given index
+l1.insertAt(20, 10);
+console.log('After Inserting the Node', l1);
+
+// Clear the list
+l1.clear();
+console.log('Link List', l1);
--- linkedList/linkedList.js
+++ linkedList/linkedList.js
@@ -23,6 +23,109 @@ class LinkedList {
     getFirst() {
         return this.head;
     }
+
+    getLast() {
+      if(!this.head) {
+        return null;
+      }
+
+      let node = this.head;
+      while(node) {
+        if(!node.next) {
+          return node;
+        }
+        node = node.next;
+      }
+    }
+
+    removeFirst() {
+      if(!this.head) {
+        return;
+      }
+
+      this.head = this.head.next;
+    }
+
+    removeLast() {
+      if(!this.head) {
+        return;
+      }
+
+      if(!this.head.next) {
+        this.head = null;
+      }
+
+      let previous = this.head;
+      let node =this.head.next;
+
+      while(node.next) {
+        previous = node;
+        node = node.next;
+      }
+
+      previous.next = null;
+    }
+
+    insertLast(item) {
+      const last = this.getLast();
+      const newNode = new Node(item);
+      if(last) {
+        last.next = newNode;
+      } else {
+        this.head = newNode;
+      }
+    }
+
+    getAt(index) {
+      let count = 0;
+      let node = this.head;
+
+      while(node) {
+        if(count === index) {
+          return node;
+        }
+        count++;
+        node = node.next;
+      }
+
+      return null;
+    }
+
+    removeAt(index) {
+      if(!this.head) {
+        return;
+      }
+
+      if(index === 0) {
+        this.head = this.head.next;
+        return;
+      }
+
+      const previous = this.getAt(index - 1);
+      if(!previous || !previous.next) {
+        return;
+      }
+      previous.next = previous.next.next;
+    }
+
+    insertAt(data, index) {
+      if(!this.head) {
+        this.head = new Node(data);
+        return;
+      }
+
+      if(index === 0) {
+        this.head = new Node(data, this.head);
+        return;
+      }
+
+      const previous = this.getAt(index - 1) || this.getLast();
+      previous.next = new Node(data, previous.next);
+    }
+
+    clear() {
+      this.head = null;
+    }
 }
 
 module.exports = LinkedList;
